260f84859a77a44ffc83c866eb0c3a504b1cd05b
dev099-coder <75147290+dev099-coder@users.noreply.github.com>
2020-12-27 14:52:34 -0300

    Add files via upload

---
+++ scr/help.js
@@ -0,0 +1,110 @@
+const help = (prefix) => {
+	return `
+❉──────────❉
+
+BEM VINDO(A) AO MENU
+
+❉──────────❉
+
+Menu simples e facil de entender 😎👍
+
+──❉ *Comandos para figurinhas* ❉──
+
+➸ Comando : *${prefix}sticker* ou *${prefix}stiker*
+➸ útil em : converter imagem/gif/vídeo em adesivo
+➸ uso : responder imagem/gif/video ou enviar imagem/gif/video com legenda\n
+
+➸ Comando : *${prefix}sticker nobg* ou *${prefix}stiker nobg*
+➸ útil em : converter imagem em adesivo removendo o fundo
+➸ uso : responder imagem ou enviar imagem com legenda/n
+
+➸ Comando : *${prefix}toimg*
+➸ útil em : converter adesivo em imagem
+➸ uso : adesivo de resposta\n
+
+➸ Comando : *${prefix}tsticker* ou *${prefix}tstiker*
+➸ útil em : converter texto em adesivo
+➸ uso : *${prefix}tsticker seu texto aqui*\n
+
+──❉ *Comandos memes* ❉──
+
+➸ Comando : *${prefix}meme*
+➸ útil em : mandar imagens aleatórias de meme [inglês]
+➸ uso : basta emviar o comando\n
+
+➸ Comando : *${prefix}memeindo*
+➸ útil em : mandar imagens aleatórias de meme [indo]
+➸ uso : basta enviar o comando\n
+
+──❉ *Outros comandos* ❉──
+
+➸ Comando : *${prefix}gtts*
+➸ útil em : converter texto em fala/áudio
+➸ uso : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja On2-chan*\n
+
+➸ Comando : *${prefix}loli*
+➸ útil em : mandar imagens aleatórias de loli
+➸ uso : basta enviar o comando\n
+
+➸ Comando : *${prefix}nsfwloli*
+➸ útil em : mandar imagens aleatórias de nsfw loli
+➸ uso : basta enviar o comando\n
+
+➸ Comando : *${prefix}url2img*
+➸ útil em : tirar screenshots da web
+➸ uso : *${prefix}url2img [tipe] [url]*\n
+➸ Comando : *${prefix}simi*
+➸ útil em : responder sua mensagem por simi
+➸ uso : *${prefix}simi sua mensagem*\n
+
+➸ Comando : *${prefix}ocr*
+➸ útil em : pegar o texto da foto e lhe enviar
+➸ uso : responder imagem ou enviar mensagem com legenda\n
+
+➸ Comando : *${prefix}wait*
+➸ útil em : pesquisar sobre o anime por imagem [ Que anime é este/que ]
+➸ uso : responder imagem ou enviar imagem com legenda\n
+
+➸ Comando : *${prefix}setprefix*
+➸ útil em : alterar o prefixo do bot
+➸ uso : *${prefix}setprefix [texto|opcional]*\nexemplo : *${prefix}setprefix ?*
+➸ Nota : Este comando só pode ser usado pelo proprietário do bot\n
+
+──❉ *Comandos de grupo* ❉──
+
+Comando : *${prefix}linkgroup*
+➸ útil em : enviar o link do grupo
+➸ uso : basta enviar o comando\n
+
+➸ Comando : *${prefix}tagall*
+➸ útil em : marcar todos os membros do grupo, incluindo administradores
+➸ uso : basta enviar o comando\n
+➸ Nota : Este comando só pode ser usado se você for um administrador do grupo\n
+
+➸ Comando : *${prefix}simih*
+➸ útil em : ativar o modo simi no grupo
+➸ uso : *${prefix}simih 1* para ativar o modo simi e *${prefix}simih 0* para desativar o modo simi
+➸ Nota : Este comando só pode ser usado se você for um administrador do grupo\n
+
+──❉ *Alguns comandos variados* ❉──
+
+➸ *${prefix}promote @*
+➸ *${prefix}demote @*
+➸ *${prefix}kick @*
+➸ *${prefix}welcome 1*
+➸ *${prefix}welcome 0*
+➸ *${prefix}clearall* (deleta todos os chats do wpp do bot)
+➸ *${prefix}listadmins (lista dos admins)
+➸ *${prefix}add (numero da pessoa)*
+
+
+
+
+╠════════════════════
+║ 📍 TRADUZIDO POR DEV JAVA📍
+╚════════════════════`
+}
+
+exports.help = help
+
+
